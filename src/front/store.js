f7d45691ac46b4a1c94a73ac083da8acf428db92
Carlos Lorenzo <carloren.1996@gmail.com>
2025-08-06 09:35:59 +0000

    06-08

--- src/front/store.js
+++ src/front/store.js
@@ -1,38 +1,35 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    isAuth: false
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'set_hello':
-      return {
-        ...store,
-        message: action.payload
-      };
-      
+  switch (action.type) {
     case 'add_task':
 
-      const { id,  color } = action.payload
+      const { id, color } = action.payload
 
       return {
         ...store,
         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
       };
+
+    case 'LOGIN':
+
+      return {
+        ...store,
+        isAuth: true
+      };
+
+    case 'LOGOUT':
+
+      return {
+        ...store,
+        isAuth: false
+      };
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
